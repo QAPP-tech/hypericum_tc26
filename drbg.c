9b980418745e073658c1bb1c20d32b41d6f492c1
Kirill Konevets <kkonevets@gmail.com>
2023-09-26 14:20:53 +0300

    use streebog for pseudorandomness

--- drbg.c
+++ drbg.c
@@ -19,6 +19,26 @@ void randombytes_init(uint8_t* entropy_input)
     }
 }
 
+// get entropy from hardware or iteratively deduce from seed
+static int get_entropy(
+    const hash_algo_t hash_algo, hash_function_ctx_new_t ctx, void* data)
+{
+    int ret = 0;
+    if (1 == DRBG_ctx.is_hardware_based) {
+        ret = get_hardware_entropy(data, DRBG_INIT_BYTES_LEN);
+    } else {
+        // iteratively update state
+        hash_algo->ctx_init(ctx);  // clean state
+        hash_algo->ctx_update(
+            ctx, DRBG_ctx.entropy_source, DRBG_INIT_BYTES_LEN);
+        hash_algo->ctx_final(ctx, DRBG_ctx.entropy_source);  // compute hash
+
+        // copy current pseudorandomness
+        memcpy(data, DRBG_ctx.entropy_source, DRBG_INIT_BYTES_LEN);
+    }
+    return ret;
+}
+
 // increment big integer, least significant bit is the right most
 // { 0x00, 0x00, 0xFF, 0x00 } -> {0x00, 0x00, 0xFF, 0x01}, return 0
 // { 0x00, 0x00, 0xFF, 0xFF } -> {0x00, 0x01, 0x00, 0x00}, return 0
@@ -34,22 +54,6 @@ static int increment(uint8_t* big_integer, size_t len)
         overflow = c == UINT8_MAX;
     }
     return overflow;
-}
-
-// get entropy from hardware or iteratively deduce from seed
-static int get_entropy(void* data)
-{
-    int ret = 0;
-    if (1 == DRBG_ctx.is_hardware_based) {
-        ret = get_hardware_entropy(data, DRBG_INIT_BYTES_LEN);
-    } else {
-        // iteratively update state
-        increment(DRBG_ctx.entropy_source, DRBG_INIT_BYTES_LEN);
-
-        // copy current pseudorandomness
-        memcpy(data, DRBG_ctx.entropy_source, DRBG_INIT_BYTES_LEN);
-    }
-    return ret;
 }
 
 static int step(
@@ -84,9 +88,9 @@ int randombytes(const hash_algo_t hash_algo, uint8_t* x, size_t xlen)
 
     memset(x, 0, xlen);
 
-    if (0 != get_entropy(u)) {
-        hash_algo->ctx_free(ctx);
-        return 1;
+    int ret = 0;
+    if ((ret = get_entropy(hash_algo, ctx, u)) != 0) {
+        goto cleanup;
     }
 
     memset(u + DRBG_INIT_BYTES_LEN, 0, sizeof(u) - DRBG_INIT_BYTES_LEN);
@@ -94,23 +98,22 @@ int randombytes(const hash_algo_t hash_algo, uint8_t* x, size_t xlen)
     if (q != 0) {  // step 4
         for (size_t i = 0; i < q; ++i) {
             x_ptr -= hash_algo->output_size;  // C_i || R
-            if (0 != step(hash_algo, ctx, u, sizeof(u), x_ptr)) {
-                hash_algo->ctx_free(ctx);
-                return 1;
+            if ((ret = step(hash_algo, ctx, u, sizeof(u), x_ptr)) != 0) {
+                goto cleanup;
             }
         }
     }
 
     if (r != 0) {  // step 5
         ALLOC_ON_STACK(uint8_t, tmp, hash_algo->output_size);
-        if (0 != step(hash_algo, ctx, u, sizeof(u), tmp)) {
-            hash_algo->ctx_free(ctx);
-            return 1;
+        if ((ret = step(hash_algo, ctx, u, sizeof(u), tmp)) != 0) {
+            goto cleanup;
         }
         // r least significant bytes
         memcpy(x, tmp + sizeof(tmp) - r, r);
     }
 
+cleanup:
     hash_algo->ctx_free(ctx);
-    return 0;
+    return ret;
 }
