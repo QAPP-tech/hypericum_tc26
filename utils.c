2313b2d8878b328f5f80c2979fb9e7c89925ec35
Yaroslav Lelkin <ylelkin@qapp.tech>
2025-10-16 16:43:05 +0500

    reverse the byte order Current branch: feature/4836-spec-2-2

--- utils.c
+++ utils.c
@@ -52,10 +52,9 @@ struct Node* hypericum_create_node(uint32_t h)
 
 void fill_bytes32(uint8_t* bytes, uint32_t value)
 {
-    // big endian
-    bytes[0] = value >> 8 * 3 & 0xFF;
-    bytes[1] = value >> 8 * 2 & 0xFF;
-    bytes[2] = value >> 8 & 0xFF;
-    bytes[3] = value & 0xFF;
+    bytes[0] = value & 0xFF;
+    bytes[1] = (value >> 8) & 0xFF;
+    bytes[2] = (value >> 16) & 0xFF;
+    bytes[3] = (value >> 24) & 0xFF;
 }
 
